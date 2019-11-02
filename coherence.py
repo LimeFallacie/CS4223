94023adc4710556667759f8747bcfcbc45ed4d59
LimeFallacie\Emil <e0014667@u.nus.edu>
2019-11-02 16:02:44 +0800

    some parser stuff

---
+++ coherence.py
@@ -0,0 +1,11 @@
+import sys
+
+# script arguments parsed here [coherence “protocol” “input_file” “cache_size” “associativity” “block_size”]
+
+print("protocol = %s" % sys.argv[1])
+print("input file = %s" % sys.argv[2])
+print("cache size = %s" % sys.argv[3])
+print("assoc = %s" % sys.argv[4])
+print("block size = %s" % sys.argv[5])
+
+
