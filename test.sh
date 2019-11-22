9b21a2cd6793df87297e1214398313f5a9af6044
LimeFallacie\Emil <e0014667@u.nus.edu>
2019-11-22 16:32:26 +0800

    update test.sh to include MOESI

--- test.sh
+++ test.sh
@@ -14,3 +14,11 @@ python coherence.py dragon rdafterwr 1024 1 16
 python coherence.py dragon rdrdhit 1024 1 16
 python coherence.py dragon rdsame 1024 1 16
 python coherence.py dragon rdsameblock 1024 1 16
+echo ==========================================
+echo ===================MOESI==================
+echo ==========================================
+python coherence.py MOESI evict 1024 1 16
+python coherence.py MOESI rdafterwr 1024 1 16
+python coherence.py MOESI rdrdhit 1024 1 16
+python coherence.py MOESI rdsame 1024 1 16
+python coherence.py MOESI rdsameblock 1024 1 16
